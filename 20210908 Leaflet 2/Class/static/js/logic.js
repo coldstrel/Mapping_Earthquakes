8adf40713b551a8af0c08985ad6b3acbf9608d87
coldstrel <aldofra98@gmail.com>
2021-09-11 15:17:12 -0500

    Adding style

--- 20210908 Leaflet 2/Class/static/js/logic.js
+++ 20210908 Leaflet 2/Class/static/js/logic.js
@@ -32,80 +32,98 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 //Accesing the airport geoJSON
 let airportData = "https://raw.githubusercontent.com/coldstrel/Mapping_Earthquakes/Simple_Leaf_Map/20210908%20Leaflet%202/Class/majorAirports.json";
-
+let torontoData = "https://raw.githubusercontent.com/coldstrel/Mapping_Earthquakes/Simple_Leaf_Map/20210908%20Leaflet%202/Class/torontoRoutes.json";
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-// An array containing each city's location, state, and population.
-let cityData = cities;
-// Loop through the cities array and create one marker for each city.
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, { 
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// //  Add a marker to the map for Los Angeles, California.
+// // An array containing each city's location, state, and population.
+// let cityData = cities;
+// // Loop through the cities array and create one marker for each city.
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, { 
+//         radius: city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
 
 // Coordinates for each point to be used in the line.
 // Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow"
-  }).addTo(map);
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+//   ];
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "yellow"
+//   }).addTo(map);
 
   // Create the map object with center at the San Francisco airport.
 // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
 // Grabbing our GeoJSON data.
 
-L.geoJSON(sanFranAirport).addTo(map);
-// Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    pointToLayer: function(feature, latlng) {
-      console.log(feature);
-      return L.marker(latlng)
-      .bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2><hr> <br> Airport Name: <h4>" + feature.properties.name +"</h4>")
-    }
+// L.geoJSON(sanFranAirport).addTo(map);
+// // Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+//       return L.marker(latlng)
+//       .bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2><hr> <br> Airport Name: <h4>" + feature.properties.name +"</h4>")
+//     }
 
-  }).addTo(map);
+//   }).addTo(map);
+
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data,{
+//       pointToLayer: function(feature,latlng) {
+//           return L.marker(latlng)
+//           .bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2><hr> <br> Airport Name: <h4>" + feature.properties.name +"</h4>")
+//       }
+//   }).addTo(map);
+// });
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data,{
-      pointToLayer: function(feature,latlng) {
-          return L.marker(latlng)
-          .bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2><hr> <br> Airport Name: <h4>" + feature.properties.name +"</h4>")
+  L.geoJson(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: " 
+          + feature.properties.dst + "</h3>");
       }
   }).addTo(map);
 });
