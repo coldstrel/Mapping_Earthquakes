d8a3b5eb54d3858823e2263877ec5f1199fcdb40
coldstrel <aldofra98@gmail.com>
2021-09-11 15:37:00 -0500

    Toronto hoods

--- 20210908 Leaflet 2/Class/static/js/logic.js
+++ 20210908 Leaflet 2/Class/static/js/logic.js
@@ -8,6 +8,12 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: apiKey
 });
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: apiKey
+});
+
 
 // We create the dark view tile layer that will be an option for our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -18,14 +24,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    "Street": streets,
+    "Dark": dark,
+    "Satellite Streets": satelliteStreets
   };
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
-    zoom: 2,
-    layers: [streets]
+    center: [43.7 , -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -33,6 +40,8 @@ L.control.layers(baseMaps).addTo(map);
 //Accesing the airport geoJSON
 let airportData = "https://raw.githubusercontent.com/coldstrel/Mapping_Earthquakes/Simple_Leaf_Map/20210908%20Leaflet%202/Class/majorAirports.json";
 let torontoData = "https://raw.githubusercontent.com/coldstrel/Mapping_Earthquakes/Simple_Leaf_Map/20210908%20Leaflet%202/Class/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/coldstrel/Mapping_Earthquakes/Simple_Leaf_Map/20210908%20Leaflet%202/Class/torontoNeighborhoods.json";
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
@@ -115,15 +124,20 @@ let myStyle = {
     weight: 2
 }
 
-// Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+// // Grabbing our GeoJSON data.
+// d3.json(torontoData).then(function(data) {
+//     console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data, {
+//       style: myStyle,
+//       onEachFeature: function(feature, layer) {
+//           layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: " 
+//           + feature.properties.dst + "</h3>");
+//       }
+//   }).addTo(map);
+// });
+
+d3.json(torontoHoods).then(function (data) {
     console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-      style: myStyle,
-      onEachFeature: function(feature, layer) {
-          layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: " 
-          + feature.properties.dst + "</h3>");
-      }
-  }).addTo(map);
-});
+    L.geoJson(data).addTo(map);
+})
